82f2458c304cbec305a337e6f7b491d59f0cf1e4
backsapc <backsapce@hotmail.com>
2017-11-19 17:17:32 +0300

    Changed spot radius

--- resources/entities/enemy.js
+++ resources/entities/enemy.js
@@ -11,8 +11,8 @@ class Enemy extends Entity {
         this.canFire = true;
         this.canTestFire = true;
         this.noObstacles = false;
-        this.spotRadius = 200;
-        this.minSpotRadius = 200;
+        this.spotRadius = 2000;
+        this.minSpotRadius = 2000;
     }
 
     draw() {
